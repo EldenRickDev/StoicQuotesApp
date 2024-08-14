fe1fa58660d8560a6130edac51c2f9e0d7f067cd
EldenRickDev <EldenRickDev@gmail.com>
2024-08-14 14:38:04 -0400

    Add files via upload

---
+++ StoicQuotesApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct StoicQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // La vista principal debe ser ContentView
+        }
+    }
+}
